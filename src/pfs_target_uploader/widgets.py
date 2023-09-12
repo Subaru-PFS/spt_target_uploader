38b983d48f0b36b64ad3fd5faa7a33ff167cb835
Masato Onodera <masato.onodera@gmail.com>
2023-09-11 18:16:30 -1000

    minor fix to the link to doc

--- src/pfs_target_uploader/widgets.py
+++ src/pfs_target_uploader/widgets.py
@@ -24,7 +24,7 @@ class UploadNoteWidgets:
 class DocLinkWidgets:
     def __init__(self):
         self.doc = pn.pane.Markdown(
-            "<font size='4'><i class='fa-solid fa-circle-info fa-lg' style='color: #3A7D7E;'></i> <a href='/doc/index.html' target='_blank'>Documentation</a></font>",
+            "<font size='4'><i class='fa-solid fa-circle-info fa-lg' style='color: #3A7D7E;'></i> <a href='doc/index.html' target='_blank'>Documentation</a></font>",
             styles={"text-align": "right"},
         )
         self.pane = pn.Column(self.doc)
