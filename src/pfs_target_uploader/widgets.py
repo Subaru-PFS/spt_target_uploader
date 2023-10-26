090a60a5591ebe49b0a24694045f1c574ff126fe
Masato Onodera <masato.onodera@gmail.com>
2023-10-25 14:19:22 -1000

    change appearance of float panel for warning

--- src/pfs_target_uploader/widgets.py
+++ src/pfs_target_uploader/widgets.py
@@ -85,19 +85,20 @@ class UploadNoteWidgets:
                 ),
             ]
 
-        if not ppp_status:
+        elif not ppp_status:
             self.floatpanel = pn.layout.FloatPanel(
                 None,
                 # pn.pane.Markdown(message),
-                name="⚠️ Warning",
+                name="Warning",
                 # config={"headerLogo": "<i class='fa-regular fa-thumbs-up fa-lg'></i>"},
                 contained=False,
                 position="center",
                 # theme="none",
                 # theme="#FFF1C2",
-                theme="#866208",
+                theme="#98741E fillcolor #FFF3D0",
                 margin=20,
                 width=720,
+                # config={"theme": {"colorContent": "#866208"}},
             )
 
             # JS on-click actions
@@ -106,10 +107,15 @@ class UploadNoteWidgets:
             stylesheet = """
         :host {
             --font-size: 2.5em;
-            --color: #866208;
+            --color: #98741E;
         }
         .bk-btn-light {
-            color: #866208;
+            color: #98741E;
+            background-color: #FFF3D0;
+        }
+        .bk-btn-light:hover {
+            color: #98741E;
+            background-color: #fffacd;
         }
         """
 
@@ -133,17 +139,17 @@ class UploadNoteWidgets:
 
             messages = [
                 pn.pane.Markdown(
-                    "<i class='fa-regular fa-thumbs-up fa-2xl'></i><font size='4'>  Upload successful **_WITHOUT_** pointing simulation! Your **Upload ID** is the following.</font>"
+                    "<i class='fa-solid fa-triangle-exclamation fa-2xl' style='color: #98741E;'></i><font size='4' color='#98741E'>  Upload successful **_WITHOUT_** pointing simulation! Your **Upload ID** is the following.</font>"
                 ),
                 self.copy_source_button,
                 pn.pane.Markdown(
-                    f"<font size='4'>Uploaded at {uploaded_time.isoformat(timespec='seconds')}</font>"
+                    f"<font size='4' color='#98741E'>Uploaded at {uploaded_time.isoformat(timespec='seconds')}</font>"
                 ),
                 pn.pane.Markdown(
                     """
-                - Please keep the Upload ID for the observation planning.
-                - You can copy the Upload ID to the clipboard by clicking it.
-                - **It is not recommended to submit a target list without simulating pointings.**
+                - <font color='#98741E'>Please keep the Upload ID for the observation planning.</font>
+                - <font color='#98741E'>You can copy the Upload ID to the clipboard by clicking it.</font>
+                - <font color='#98741E'>**It is not recommended to submit a target list without a pointing simulation.**</font>
                 """
                 ),
             ]
