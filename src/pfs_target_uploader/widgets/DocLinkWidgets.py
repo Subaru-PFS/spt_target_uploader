e653b1ca06b267ef516e7843c48b59648e5a7698
Masato Onodera <masato.onodera@gmail.com>
2023-12-19 17:11:30 -1000

    wip: update vaildation doc

--- src/pfs_target_uploader/widgets/DocLinkWidgets.py
+++ src/pfs_target_uploader/widgets/DocLinkWidgets.py
@@ -6,7 +6,7 @@ import panel as pn
 class DocLinkWidgets:
     def __init__(self):
         self.doc = pn.pane.Markdown(
-            "<font size='4'><i class='fa-solid fa-circle-info fa-lg' style='color: #3A7D7E;'></i> <a href='doc/index.html' target='_blank'>Manual</a></font>",
+            "<font size='4'><i class='fa-solid fa-circle-info fa-lg' style='color: #3A7D7E;'></i> <a href='doc/index.html' target='_blank'>User Guide</a></font>",
             # "<font size='4'><i class='fa-solid fa-circle-info fa-lg' style='color: #3A7D7E;'></i> <a href='doc/index.html' target='_blank'>Documentation</a></font>",
             # styles={"text-align": "right"},
         )
