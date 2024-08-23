a32dd28e904430a0e07da02a94d40fdcf79b9433
Masato Onodera <masato.onodera@gmail.com>
2024-08-22 15:11:51 -1000

    add a missing file

---
+++ src/pfs_target_uploader/widgets/AnnouncementNoteWidgets.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+
+import panel as pn
+
+
+class AnnouncementNoteWidgets:
+    def __init__(self, ann_file=None):
+        if ann_file is not None:
+
+            with open(ann_file, "r") as f:
+                message = f.read()
+
+            self.floatpanel = pn.layout.FloatPanel(
+                message,
+                name="Important Updates for Users",
+                config={
+                    "headerLogo": "<i style='margin-left: 0.5em;' class='fa-solid fa-circle-info fa-lg'></i>",
+                },
+                contained=False,
+                position="center",
+                # theme="danger",
+                # theme="#3A7D7E",
+                theme="#DB2955",
+                margin=20,
+                # margin=100,
+                width=720,
+                # height=350,
+            )
