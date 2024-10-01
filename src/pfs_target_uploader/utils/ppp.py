0b6af17a5c97478384c322e2c8a6d0e12401083d
wanqqq31 <wanqiu.he@nao.ac.jp>
2024-10-01 17:12:55 +0900

    [trial_3] fix the brokenpipeerror (queue)

--- src/pfs_target_uploader/utils/ppp.py
+++ src/pfs_target_uploader/utils/ppp.py
@@ -55,9 +55,9 @@ def PPPrunStart(
     quiet=True,
     clustering_algorithm="HDBSCAN",
     queue=None,
-    logger_=None,
+    logger=None,
 ):
-    if logger_ is None:
+    if logger is None:
         logger.remove()
         logger.add(sys.stderr, level="INFO", enqueue=True)
 
