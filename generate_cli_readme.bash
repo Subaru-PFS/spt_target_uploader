9efc1ebf0c8f5e71b76d46aeb93da78a1cd22329
Masato Onodera <masato.onodera@gmail.com>
2025-10-31 09:09:07 -1000

    remove generate_cli_readme.bash

--- generate_cli_readme.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-# shellcheck disable=SC2016
-typer pfs_target_uploader.cli.cli_main utils docs --name pfs-uploader-cli |
-    sed 's/# `/## `/g' |
-    sed 's/###/---\n\n###/g' |
-    sed 's/`pfs-uploader-cli\ /`/g'
